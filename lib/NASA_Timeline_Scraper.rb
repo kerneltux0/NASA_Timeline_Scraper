13015330b3a28bab993adbb1538420396c21f1a2
Ryan <kerneltux0@gmail.com>
2019-04-26 09:41:29 -0600

    commented-out unused code in environment file

--- lib/NASA_Timeline_Scraper.rb
+++ lib/NASA_Timeline_Scraper.rb
@@ -1,7 +1,7 @@
 require_relative "./NASA_Timeline_Scraper/version"
 require_relative "./cli.rb"
 
-module NASATimelineScraper
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module NASATimelineScraper
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
