0f4f8edfa40c57d09723edaff78f777d646e592f
Ryan <kerneltux0@gmail.com>
2019-04-26 12:00:09 -0600

    moved URL to cli file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,5 @@
 class NASATimelineScraper::ScrapePg1
   #calls on Nokogiri & open-uri to parse years 1957-1983
-  url = "https://worldhistoryproject.org/topics/nasa"
   def self.scrape_pg1(url)
     nasaSite = Nokogiri::HTML(open(url))
   end
@@ -9,7 +8,7 @@ end
 
 class NASATimelineScraper::ScrapePg2
   #calls on Nokogiri & open-uri to parse years 1986-2012
-  url = "https://worldhistoryproject.org/topics/nasa/page/2"
+
   def self.scrape_pg2(url)
     nasaSite = Nokogiri::HTML(open(url))
   end
