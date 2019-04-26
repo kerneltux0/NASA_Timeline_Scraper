06c1206f24a0121a830a710ace7e861bcf0a0261
Ryan <kerneltux0@gmail.com>
2019-04-26 09:53:29 -0600

    added scraper file & updated environment file

--- lib/NASA_Timeline_Scraper.rb
+++ lib/NASA_Timeline_Scraper.rb
@@ -1,5 +1,6 @@
 require_relative "./NASA_Timeline_Scraper/version"
 require_relative "./cli.rb"
+require_relative "./scraper.rb"
 
 # module NASATimelineScraper
 #   class Error < StandardError; end
---
+++ lib/scraper.rb
@@ -0,0 +1,9 @@
+class ScrapePg1
+  #calls on Nokogiri & open-uri to parse years 1957-1983
+  
+end
+
+class ScrapePg2
+  #calls on Nokogiri & open-uri to parse years 1986-2012
+
+end
