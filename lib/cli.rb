39bab0b11a3d651eea248c9d974a2791e94b5431
Ryan <kerneltux0@gmail.com>
2019-04-26 09:51:08 -0600

    Updated cli file

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,8 @@ class NASATimelineScraper::CLI
 
   def call
     main_menu
+    user_options
+    goodbye
 
   end
 
@@ -11,6 +13,36 @@ class NASATimelineScraper::CLI
     puts "Which year range do you want to know more about?"
     puts "1: 1957 - 1983"
     puts "2: 1986 - 2012"
+
+  end
+
+  def user_options
+    puts "Please enter one of the following:"
+    puts "The number matching what you want to see"
+    puts "'menu' to return to the main menu"
+    puts "'list' to see the list"
+    puts "'exit' to exit"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "list"
+        puts "This is the list"
+      when "1"
+        puts "Al-onsey!"
+      when "2"
+        puts "Reversing the polarity of the neutron flow."
+      when "menu"
+        main_menu
+      else
+        puts "Invalid input, please try again."
+      end
+    end
+
+  end
+
+  def goodbye
+    puts "Thank you for checking out a snapshot of NASA's history!"
 
   end
 
