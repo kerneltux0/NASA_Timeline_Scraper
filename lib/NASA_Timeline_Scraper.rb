55e3c7b12f7e11adad57efb380505c7180f4133d
Ryan <kerneltux0@gmail.com>
2019-04-26 09:57:14 -0600

    updated environment file

--- lib/NASA_Timeline_Scraper.rb
+++ lib/NASA_Timeline_Scraper.rb
@@ -1,6 +1,7 @@
 require_relative "./NASA_Timeline_Scraper/version"
 require_relative "./cli.rb"
 require_relative "./scraper.rb"
+require_relative "./events.rb"
 
 # module NASATimelineScraper
 #   class Error < StandardError; end
