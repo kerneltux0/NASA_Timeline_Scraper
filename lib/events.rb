3e12286511eb1f6b38e80312f9b52b4502259640
Ryan <kerneltux0@gmail.com>
2019-04-26 11:21:03 -0600

    removed test code & commented-out unused method

--- lib/events.rb
+++ lib/events.rb
@@ -12,24 +12,21 @@ class NASATimelineScraper::Events
   
   def self.events
     #return instances of events
-    event_1.headline = "Soviet Union Launches Sputnik 2"
-    event_1.date = "Nov 3 1957"
-    event_1.description = "The Soviet Union went on to launch a series of additional Sputnik satellites. Sputnik 2, for example, carried a dog into space, named Laika...."
-    event_1
-    #@@events
+    @@events
   end
 
   def save
     @@events << self
   end
 
-  def add_event(something)
+  # def add_event(something)
 
-  end
+  # end
   
 end
 
 # test data
+# event_1 = NASATimelineScraper::Events.new(headline, date, description)
 # event_1.headline = "Soviet Union Launches Sputnik 2"
 # event_1.date = "Nov 3 1957"
 # event_1.description = The Soviet Union went on to launch a series of additional Sputnik satellites. Sputnik 2, for example, carried a dog into space, named Laika....
