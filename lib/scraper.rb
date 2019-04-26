bbf6ccaf47f399a88a52c2d7b3753a63e3ed34f6
Ryan <kerneltux0@gmail.com>
2019-04-26 11:12:23 -0600

    added scraping class methods to scraper file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,17 @@
-class ScrapePg1
+class NASATimelineScraper::ScrapePg1
   #calls on Nokogiri & open-uri to parse years 1957-1983
+  url = "https://worldhistoryproject.org/topics/nasa"
+  def self.scrape_pg1(url)
+    nasaSite = Nokogiri::HTML(open(url))
+  end
   
 end
 
-class ScrapePg2
+class NASATimelineScraper::ScrapePg2
   #calls on Nokogiri & open-uri to parse years 1986-2012
+  url = "https://worldhistoryproject.org/topics/nasa/page/2"
+  def self.scrape_pg2(url)
+    nasaSite = Nokogiri::HTML(open(url))
+  end
 
 end
